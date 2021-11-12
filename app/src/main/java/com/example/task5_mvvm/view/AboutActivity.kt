49547bb7f68abc4029704ce4a7cc382d8966b833
Бадрединов Эльдар <bodriy666@gmail.com>
2021-11-12 16:02:46 +0400

    Задание 8: Добавлено определение местоположения устройства

--- app/src/main/java/com/example/task5_mvvm/view/AboutActivity.kt
+++ app/src/main/java/com/example/task5_mvvm/view/AboutActivity.kt
@@ -1,12 +1,59 @@
 package com.example.task5_mvvm.view
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.task5_mvvm.R
+import androidx.core.app.ActivityCompat
+import com.example.task5_mvvm.constants.ACCESS_FINE_LOCATION_REQUEST_CODE
+import com.example.task5_mvvm.databinding.ActivityAboutBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class AboutActivity : AppCompatActivity() {
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var binding: ActivityAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding = ActivityAboutBinding.inflate(layoutInflater).apply {
+            locationButton.setOnClickListener { v: View? ->
+                fetchLocation()
+            }
+        }
+
+        setContentView(binding.root)
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                ACCESS_FINE_LOCATION_REQUEST_CODE
+            )
+            return
+        }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+            if (it != null) {
+                Toast.makeText(
+                    applicationContext,
+                    "Your location is:\n${it.latitude}  -  ${it.longitude}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
